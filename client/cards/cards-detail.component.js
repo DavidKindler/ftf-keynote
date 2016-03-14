c74a50320cbaa8cf25d5f845f929060670368fb8
Kindler David-R12306C <David.Kindler@freescale.com>
2016-03-14 09:47:30 -0500

    updated card detail page with modal for card

--- client/cards/cards-detail.component.js
+++ client/cards/cards-detail.component.js
@@ -30,9 +30,20 @@ angular.module('ftfKeynoteApp')
   
   $scope.subscribe('cards');
   $scope.helpers({
-    card: function() {
+    cardOne: function() {
       return Cards.findOne({ _id: $stateParams.cardId }); 
-    }
+    },
+    cards: () => {
+             return Cards.find({}) 
+          },
   });
 });
   
+angular.module('ftfKeynoteApp')
+.filter('published', function () {
+  return function(cards,timeFilter) {
+    return _.filter(cards, function(card){
+       return card.time <= timeFilter
+    })
+  }
+});
