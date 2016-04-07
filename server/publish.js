a5c1c5a71642ac7cdaab7b89a6dc396ba5676775
Kindler David-R12306C <David.Kindler@freescale.com>
2016-04-07 17:09:31 -0500

    clening up twits and extra files

---
+++ server/publish.js
@@ -0,0 +1,17 @@
+'use strict'
+
+Meteor.publish("cards", function () {
+  return Cards.find({});
+});
+
+Meteor.publish("publicCards", function () {
+  let selector = {
+    $and: [{"public": true}, {"public": {$exists: true}} ]
+  };
+  return Cards.find(selector);
+});
+
+Meteor.publish('tweets', function(options) {
+  // Counts.publish(this, 'numberOfTweets', Tweets.find({}), {noReady: true});
+  return Tweets.find({}, options);
+});
