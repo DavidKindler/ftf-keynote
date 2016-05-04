de0cad1ebc1ba76ee7800de05d2ee15cabf7e477
Kindler David-R12306C <David.Kindler@freescale.com>
2016-05-04 12:45:08 -0500

    Added modal directive for card detail page

--- client/cards/cards-detail.component.js
+++ client/cards/cards-detail.component.js
@@ -1,10 +1,5 @@
 angular.module('ftfKeynoteApp')
-.controller('CardDetailCtrl', function($scope, $stateParams, $log) {
-  // $scope.$log = $log;
-  // $log.debug('stateparams:',$stateParams);
-  // $scope.cards = $rootScope.Xcards;
-  // console.log ($scope.cards);
-  // console.log ($rootScope.Xcards);
+.controller('CardDetailCtrl', function($scope, $stateParams) {
   $scope.subscribe('cards');
   $scope.id = $stateParams.cardId;
   $scope.currentUrl = 'http://www.nxp.com/event/ftf2016/day1/card';
@@ -19,4 +14,44 @@ angular.module('ftfKeynoteApp')
       return Cards.find({}) 
     }
   });
-});
+  $scope.showModal = true;
+   $scope.closeModal = function(){
+          $scope.showModal = !$scope.showModal;
+   }
+
+})
+ .directive('clientModal', function () {
+    return {
+      template:  '<div class="modal fade"><div class="modal-dialog detail"><div class="modal-content"><div class="modal-body" ng-transclude></div></div></div></div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+          { 
+            $(element).modal({'show': true, 'backdrop':'static'});
+          }
+          else {
+            $(element).modal('hide');
+          }
+        });
+       
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+
+      }
+    };
+  });
