007f9ee48ba13a42d18831d4e10e7357e0462b3e
Kindler David-R12306C <David.Kindler@freescale.com>
2016-05-03 20:37:07 -0500

    added prerender-node, removed iron router

--- server/prerender.js
+++ server/prerender.js
@@ -1,5 +1,8 @@
 // Use Prerender with your token
 var prerenderio = Npm.require('prerender-node').set('prerenderToken', '28Ox9IDvKnZHjdhfD2hQ'); 
-// var prerenderio= Npm.require('prerender-node').set('prerenderServiceUrl', 'http:'));
+
+// var prerenderio= Npm.require('prerender-node')
+// prerenderio.set('prerenderServiceUrl', 'http://localhost:3000/');
+// prerenderio.set('prerenderToken', '28Ox9IDvKnZHjdhfD2hQ');
 // Feed it to middleware! (app.use)
 WebApp.connectHandlers.use(prerenderio);
